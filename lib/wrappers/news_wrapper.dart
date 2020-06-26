3268ebb9beb7bc6daffb8903142f1edfdb7649ae
Eroo36 <edogruca@hotmail.com>
2020-06-27 02:10:22 +0300

    news_wrapper.dart

--- lib/wrappers/news_wrapper.dart
+++ lib/wrappers/news_wrapper.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 
 class NewsWrapper extends StatelessWidget {
-  final List<String> newsTitle = [
-    'title1ASDASDASDadsadsadsadsadsadsadsadsadsads',
-    'title2ASDASDASDadsadsadsadsads',
-    'title3ASDASDASDdsaaaaaaaa'
+  final List<Map<String, String>> liste = [
+    {
+      'title': 'Başlık',
+      'imageUrl': 'assets/images/slide04.jpg',
+      'subTitle':
+          'Altyazıasddddddddddddddddddassadsadaaaaaaaaaaaaaadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsads'
+    },
+    {
+      'title': 'Başlık',
+      'imageUrl': 'assets/images/slide04.jpg',
+      'subTitle':
+          'Altyazıasddddddddddddddddddassadsadaaaaaaaaaaaaaadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsads'
+    },
+    {
+      'title': 'Başlık',
+      'imageUrl': 'assets/images/slide04.jpg',
+      'subTitle':
+          'Altyazıasddddddddddddddddddassadsadaaaaaaaaaaaaaadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsads'
+    }
   ];
-  final List<String> newsImage = [
-    'assets/images/slide04.jpg',
-    'assets/images/slide04.jpg',
-    'assets/images/slide04.jpg'
-  ];
-  final List<String> newsSubTitle = ['Subtitle1', 'Subtitle2', 'Subtitle3'];
+
+  // final List<String> newsTitle = [
+  //   'title1ASDASDASDadsadsadsadsadsadsadsadsadsads',
+  //   'title2ASDASDASDadsadsadsadsads',
+  //   'title3ASDASDASDdsaaaaaaaa'
+  // ];
+  // final List<String> newsImage = [
+  //   'assets/images/slide04.jpg',
+  //   'assets/images/slide04.jpg',
+  //   'assets/images/slide04.jpg'
+  // ];
+
+  // final List<String> newsSubTitle = [
+  //   'Subtitle1adsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsads',
+  //   'Subtitle2',
+  //   'Subtitle3'
+  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,40 +48,75 @@ class NewsWrapper extends StatelessWidget {
       body: Container(
         child: ListView.builder(
           physics: BouncingScrollPhysics(),
-          itemCount: newsTitle.length,
+          itemCount: liste.length,
           itemBuilder: (BuildContext context, int i) {
-            return Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(),
-                  padding: EdgeInsets.only(bottom: 100),
-                  height: 300,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      ListTile(
-                        leading: GestureDetector(
-                          onTap: null,
-                          child: Image.asset(newsImage[i]),
+            return InkWell(
+              onTap: () => null,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                elevation: 4,
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        ClipRRect(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15),
+                          ),
+                          child: Image.asset(
+                            liste[i]['imageUrl'],
+                            height: 250,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  // margin: EdgeInsets.symmetric(horizontal: 80),
-                  // padding: EdgeInsets.only(bottom: 50),
-                  height: 100,
-                  child: ListTile(
-                    title: Text(
-                      newsTitle[i],
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        Positioned(
+                          bottom: 20,
+                          right: 10,
+                          child: Container(
+                            width: 300,
+                            color: Colors.black54,
+                            padding: EdgeInsets.symmetric(
+                              vertical: 5,
+                              horizontal: 20,
+                            ),
+                            child: Text(
+                              liste[i]['title'],
+                              style: TextStyle(
+                                fontSize: 26,
+                                color: Colors.white,
+                              ),
+                              softWrap: true,
+                              overflow: TextOverflow.fade,
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                    subtitle: Text(newsSubTitle[i]),
-                  ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Flexible(
+                            child: Text(
+                              liste[i]['subTitle'], //.substring(0, 95) + '...',
+                              style: TextStyle(fontSize: 15),
+                              maxLines: 3,
+                              softWrap: true,
+                              overflow: TextOverflow.fade,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             );
           },
         ),
