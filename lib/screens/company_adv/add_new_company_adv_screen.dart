c215be1c51cdc42e78d097c9f8407122353ced23
meroo36 <a7xlife@hotmail.com>
2020-07-10 14:12:49 +0200

    commit lines deleted add_new_company_adv_screen.dart

--- lib/screens/company_adv/add_new_company_adv_screen.dart
+++ lib/screens/company_adv/add_new_company_adv_screen.dart
@@ -150,48 +150,4 @@ class _AddNewCompanyAdvScreenState extends State<AddNewCompanyAdvScreen> {
       ),
     );
   }
-  // File _image;
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   Future getImage() async {
-  //     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-
-  //     setState(() {
-  //       _image = image;
-  //       print(_image);
-  //     });
-  //   }
-
-  //   Future uploadPicture(BuildContext ctx) async {
-  //     String fileName = basename(_image.path);
-  //     StorageReference firebaseStorageRef =
-  //         FirebaseStorage.instance.ref().child(fileName);
-  //     StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
-  //     print(uploadTask);
-  //     print(firebaseStorageRef);
-
-  //     StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
-  //   }
-
-  // return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text("Yeni İlan Ekle"),
-  //     ),
-  //     body: Column(
-  //       children: <Widget>[
-  //         _image == null
-  //             ? Text("boş")
-  //             : Image.file(
-  //                 _image,
-  //                 width: 500,
-  //                 height: 500,
-  //               ),
-  //         IconButton(icon: Icon(Icons.add_a_photo), onPressed: getImage),
-  //         IconButton(
-  //             icon: Icon(Icons.file_upload),
-  //             onPressed: () => uploadPicture(context)),
-  //       ],
-  //     ));
-  //}
 }
